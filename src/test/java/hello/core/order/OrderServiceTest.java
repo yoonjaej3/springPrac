b94dc9710e22ce9b5dfcb5b96c0971f3c116055a
yoonjaej3 <yoonjaej3@gmail.com>
2022-05-01 16:50:08 +0900

    getBean

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -32,5 +32,6 @@ class OrderServiceTest {
         memberService.join(member);
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+
     }
 }
